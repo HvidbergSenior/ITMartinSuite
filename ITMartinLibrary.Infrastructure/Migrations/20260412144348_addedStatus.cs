dc4f6819836d3e0f7cd710368207cfbae27bef52
Martin Hvidberg <hvidbergsenior@gmail.com>
2026-04-12 21:49:55 +0200

    added stats

---
+++ ITMartinLibrary.Infrastructure/Migrations/20260412144348_addedStatus.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITMartinLibrary.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DetailsUpdatedAt",
+                table: "Items",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LookupStatus",
+                table: "Items",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DetailsUpdatedAt",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "LookupStatus",
+                table: "Items");
+        }
+    }
+}
