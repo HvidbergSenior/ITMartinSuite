9fd50b04b5faf31d9cc0ec6e4b91a94b3a6713ba
Martin Hvidberg <hvidbergsenior@gmail.com>
2026-04-18 11:53:11 +0200

    budget initial

---
+++ ITMartinBudget.Application/Services/BudgetService.cs
@@ -0,0 +1,19 @@
+﻿
+using ITMartinBudget.Domain.Entities;
+
+namespace ITMartinBudget.Application.Services;
+
+public class BudgetService
+{
+    public IEnumerable<object> GetSummary(IEnumerable<Transaction> data, int year)
+    {
+        return data
+            .Where(x => x.Date.Year == year)
+            .GroupBy(x => x.Category)
+            .Select(g => new
+            {
+                Category = g.Key,
+                Total = g.Sum(x => x.Amount)
+            });
+    }
+}
---
+++ ITMartinBudget.Infrastructure/BudgetContext.cs
@@ -0,0 +1,12 @@
+﻿using ITMartinBudget.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITMartinBudgetInfrastructure;
+
+public class BudgetDbContext : DbContext
+{
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=budget.db");
+}
