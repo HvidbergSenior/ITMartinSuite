3c503d3a5e2e9108b6d0077289ab88b6bb0bacc0
Martin Hvidberg <hvidbergsenior@gmail.com>
2026-04-24 10:18:37 +0200

    compose

---
+++ ITMartinImageProcessor.Worker/Program.cs
@@ -0,0 +1,28 @@
+﻿using ITMartinImageProcessor.Application.Handlers;
+using ITMartinImageProcessor.Application.Services;
+using ITMartinImageProcessor.Domain.Interfaces;
+using ITMartinImageProcessor.Infrastructure;
+using ITMartinImageProcessor.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+services.AddScoped<IImageProcessor, ImageSharpProcessor>();
+services.AddScoped<IFileService, FileService>();
+services.AddScoped<ProcessNewImagesHandler>();
+services.AddScoped<HeicToJpgConverterService>();
+var provider = services.BuildServiceProvider();
+
+while (true)
+{
+    using var scope = provider.CreateScope();
+    var handler = scope.ServiceProvider.GetRequiredService<ProcessNewImagesHandler>();
+
+    await handler.ExecuteAsync(
+        "/data/input",
+        "/data/ready",
+        "/data/archive"
+    );
+
+    await Task.Delay(5000); // every 5 seconds
+}
