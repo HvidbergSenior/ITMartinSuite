a041db62f57517f9306be49686fd9e51e7d3ea04
Martin Hvidberg <hvidbergsenior@gmail.com>
2026-04-30 20:05:34 +0200

    migr

---
+++ ITMartinBudget.Infrastructure/Migrations/20260430151853_addPropertyOnTransaction.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITMartinBudget.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addPropertyOnTransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TransactionType",
+                table: "Transactions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedDescription",
+                table: "Transactions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NormalizedDescription",
+                table: "Transactions");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TransactionType",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
