ead985d69563d203c42e3302ce0485479183af6b
Martin Hvidberg <hvidbergsenior@gmail.com>
2026-05-18 23:33:22 +0200

    Migration

---
+++ ITMartin.Media.Infrastructure/Persistence/Migrations/20260518210307_AddWorkflowCompletion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITMartin.Media.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWorkflowCompletion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "WorkflowResumes",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "WorkflowResumes");
+        }
+    }
+}
