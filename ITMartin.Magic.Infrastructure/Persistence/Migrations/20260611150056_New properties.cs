41713c2638c64967faa35a421d1c5ca4fa5fd1f0
Martin Hvidberg <hvidbergsenior@gmail.com>
2026-06-15 16:19:35 +0200

    Migrations

---
+++ ITMartin.Magic.Infrastructure/Persistence/Migrations/20260611150056_New properties.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITMartin.Magic.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Newproperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CopyrightYear",
+                table: "Sets",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FrameStyle",
+                table: "Sets",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SymbolColor",
+                table: "Sets",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SymbolShape",
+                table: "Sets",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CopyrightYear",
+                table: "Sets");
+
+            migrationBuilder.DropColumn(
+                name: "FrameStyle",
+                table: "Sets");
+
+            migrationBuilder.DropColumn(
+                name: "SymbolColor",
+                table: "Sets");
+
+            migrationBuilder.DropColumn(
+                name: "SymbolShape",
+                table: "Sets");
+        }
+    }
+}
